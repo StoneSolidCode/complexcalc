b3ba3d688b6b84b257c73f586b7d7753b0486abd
StoneSolidCode <chesdude@gmail.com>
2018-10-08 14:50:54 -0700

    prog up to line 30

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -12,18 +12,26 @@ import Foundation
 class Calculator {
     
     
-    func add(lhs: Int, rhs: Int) -> Int {
-        return Int(lhs + rhs)
+    func add(_ all: [Int] = [0], lhs: Int = 0, rhs: Int = 0) -> Int {
+        return Int(lhs + rhs) + all.reduce(0, +)
     }
     func subtract(lhs: Int, rhs: Int) -> Int{
         return Int(lhs - rhs)
     }
-    func multiply(lhs: Int, rhs: Int) -> Int {
-        return Int(lhs * rhs)
+    func multiply(_ all: [Int] = [0], lhs: Int = 0, rhs: Int = 0) -> Int {
+        return Int(lhs * rhs) + all.reduce(1, *)
     }
     func divide(lhs: Int, rhs: Int) -> Int {
         return Int(lhs / rhs)
     }
+    func mathOp(lhs: Int = 0, rhs: Int = 0, args: [Int] = [0], beg: Int = 0,
+                    op : (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    func count(_ args: [Int]) -> Int {
+        return args.count
+    }
+    
     
     init() {
     }
